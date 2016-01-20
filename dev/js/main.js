3a3ceaddc87de3095613943544dec60adc230798
virzen <wiktor.czajkowski@gmail.com>
2016-01-20 21:34:52 +0100

    Create basic model data object, html elements object and helpers

---
+++ dev/js/main.js
@@ -0,0 +1,68 @@
+(function (doc, win) {
+	'use strict';
+
+	helpers: {
+		// ES6 `Array.from()` polyfill
+		if (!Array.prototype.from) {
+			Array.prototype.from = function (pseudoArray) {
+				return Array.prototype.slice.call(pseudoArray);
+			};
+		}
+
+		var $ = function (selector, root = document) {
+			if (selector) {
+				let elements = root.querySelectorAll(selector);
+				return (elements.length === 1) ?
+				elements[0] :
+				Array.from(elements);
+			}
+			else {
+				return null;
+			}
+		};
+	}
+
+	// application's singleton
+	let app = (function () {
+		// private part
+		let data = {
+			timers: [
+				{
+					id: 0,
+					name: 'pomodoro',
+					length: [25, 0]
+				},
+				{
+					id: 1,
+					name: 'short break',
+					length: [5, 0]
+				},
+				{
+					id: 2,
+					name: 'long break',
+					length: [20, 0]
+				}
+			],
+			lastId: 2,
+			currentTimerId: 0,
+		};
+
+		let appBody = $('.app-body');
+		let elems = {
+			timer: $('.timer', appBody),
+			timersList: $('.timers-list', appBody),
+			buttons: {
+				startStop: $('start-stop-button', appBody),
+				reset: $('reset-button', appBody),
+			},
+		};
+
+
+		// public part
+		return {
+
+		};
+	}());
+
+	win.app = app;
+})(document, window);
