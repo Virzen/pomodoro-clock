e5df3620db25e4527f5858e360202d838be3e687
virzen <wiktor.czajkowski@gmail.com>
2016-01-21 13:34:14 +0100

    Fix timer resetting. Refactor setting current timer

--- dev/js/main.js
+++ dev/js/main.js
@@ -53,9 +53,10 @@
 				}
 			],
 			lastId: 2,
-			currentTimer: null,
-			currentTimerId: null,
-			currentTimerInterval: null,
+		};
+
+		let state = {
+			currentTimer: null
 		};
 
 		// DOM elements
@@ -79,10 +80,32 @@
 			timers: [],
 		};
 
-		// methods
+		// controller
 		////////////////////////
 		let decrementDuration = function (durArr) {
+			// reverse array for easier access
+			let revArr = durArr.reverse();
+			let len = revArr.length;
 
+			// TODO: validate durArr
+
+			for (let i = 0; i < len; i++) {
+				if (revArr[i] > 0) {
+					revArr[i] -= 1;
+					durArr = revArr.reverse();
+					return true;
+				}
+				else if (revArr[i + 1] && revArr[i + 1] > 0) {
+					revArr[i] = 59;
+				}
+			}
+
+			return false;
+		};
+
+		// TODO: Implement real signalization for timer end
+		let signalizeTimerEnd = function () {
+			alert('Timer ended!');
 		};
 
 		let setCurrentTimer = function (timerId, callback = renderMainTimer) {
@@ -90,8 +113,15 @@
 			let timerIdNum = parseInt(timerId, 10);
 			let currentTimer = data.timers.find(timer => timer.id === timerIdNum);
 
-			data.currentTimerId = timerIdNum;
-			data.currentTimer = Object.assign({}, currentTimer);
+			// copy timer object to state object
+			state.currentTimer = Object.assign({}, currentTimer);
+
+			// in result of the operation above, `duration` array is assigned
+			// instead of copied, so `state.currentTimer.duration` contains
+			// just a reference to the `currentTimer.duration`
+			// this behaviour is undesirable, so the line below overrides
+			// the referenced array to a copied one
+			state.currentTimer.duration = Array.from(currentTimer.duration);
 
 			if (callback) {
 				callback();
@@ -100,59 +130,65 @@
 
 		let stopTimer = function () {
 			// stop current timer by clearing its interval
-			clearInterval(data.currentTimerInterval);
+			if (state.currentTimer.interval) {
+				clearInterval(state.currentTimer.interval);
+			}
+		};
+
+		let endTimer = function () {
+			stopTimer();
+			signalizeTimerEnd();
 		};
 
 		let startTimer = function (timerId, callback = renderMainTimer) {
-			let currentTimer = (timerId) ?
-					data.timers.find(timer => timer.id === timerId) :
-					data.currentTimer;
+			// let currentTimer = (timerId) ?
+			// 		data.timers.find(timer => timer.id === timerId) :
+			// 		state.currentTimer;
 
 			// TODO: validate duration array
 
-			data.currentTimerInterval = setInterval(() => {
-				if (!decrementDuration(currentTimer.duration)) {
-					stopTimer();
+			state.currentTimer.interval = setInterval(() => {
+				if (!decrementDuration(state.currentTimer.duration)) {
+					endTimer();
 				}
 				else {
 					if (callback) {
 						callback();
 					}
 				}
-			}, 1000);
+			}, 10);
 		};
 
-		let resetTimer = function (timerId = data.currentTimerId, callback = renderMainTimer) {
+		let resetTimer = function (timerId = state.currentTimer.id, callback = renderMainTimer) {
 			stopTimer();
 
 			// find prototype of the current timer by id
 			let currentTimerProto = data.timers.find(timer => timer.id === timerId);
 
 			// set current timer's duration to its protoype's one
-			data.currentTimer.duration = currentTimerProto.duration;
+			state.currentTimer.duration = Array.from(currentTimerProto.duration);
 
 			if (callback) {
 				callback();
 			}
 		};
 
-		let renderMainTimer = function (timerId = data.currentTimerId) {
-			// search for timer object with given id
-			let currentTimer = data.currentTimer ||
+		// view
+		//////////////////////////
+		let renderMainTimer = function (timerId = state.currentTimer.id) {
+			// use current timer or search for timer object with given id
+			let currentTimer = state.currentTimer ||
 					data.timers.find(timer => timer.id === timerId);
 
 			if (currentTimer) {
 				// set name and time to currentTimer's ones
 				elems.mainTimer.name.textContent = currentTimer.name;
 				elems.mainTimer.time.textContent =
-					`${currentTimer.duration[0] || '00'}:
-					${currentTimer.duration[1] || '00'}`;
+					`${state.currentTimer.duration[0] || '00'}:
+					${state.currentTimer.duration[1] || '00'}`;
 
 			} else {
-				throw {
-					name: 'Error',
-					message: `Given timer doesn't exist.`
-				};
+				throw new Error(`Given timer doesn't exist.`);
 			}
 		};
 
@@ -175,7 +211,7 @@
 			timersList.appendChild(li);
 
 			// add button to the elems object
-			// FIXME: refactor this to callback passed as argument
+			// FIXME: refactor this to be callback passed as argument
 			elems.timers.push(button);
 		};
 
@@ -203,7 +239,7 @@
 			});
 
 			// set default timer as current one and trigger initial rendering
-			// of main timer
+			// of main timer (default callback)
 			setCurrentTimer(0);
 
 		};
